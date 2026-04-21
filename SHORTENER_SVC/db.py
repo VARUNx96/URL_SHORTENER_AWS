8c82a4a475d3b77141c017ad2774cbfc76f6c3db
VARUN J <70467762+VARUNx96@users.noreply.github.com>
2026-04-21 14:33:09 +0530

    S_SVC/DB.PY

---
+++ SHORTENER_SVC/db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect("urls.db",check_same_thread = False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS urls (
+               code TEXT PRIMARY KEY,
+               long_url TEXT
+               )""")
+conn.commit()
+def save_url(code, long_url):
+    cursor.execute("INSERT INTO urls (code, long_url) VALUES (?,?)", (code, long_url))
+    conn.commit()
