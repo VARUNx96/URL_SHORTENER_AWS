4bf90dce6e59cecec7f7260071b10121fe2de666
VARUN J <70467762+VARUNx96@users.noreply.github.com>
2026-04-28 12:42:29 +0530

    REDIS

--- REDIRECT_SVC/main.py
+++ REDIRECT_SVC/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI, HTTPException # type: ignore
 from fastapi.responses import RedirectResponse # type: ignore
 from db import get_url
+import redis
 
 app = FastAPI()
 
+r = redis.Redis(host="redis", port=6579, decode_responses=True)
+
 @app.get("/{code}")
 def redirect(code: str):
+    cached_url = r.get(code)
+
+    if cached_url:
+        return RedirectResponse(url = cached_url)
     long_url = get_url(code)
     if not long_url:
-        raise HTTPException(status_code=404, detail="URL NOT FOUND")
-    return RedirectResponse(long_url)
+        raise HTTPException(status_code=404, detail = "URL NOT FOUND ⚠️")
+    r.set(code, long_url)
+    return RedirectResponse(url = long_url)
--- SHORTENER_SVC/main.py
+++ SHORTENER_SVC/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI # type: ignore
 from pydantic import BaseModel # type: ignore
 from utils import genrate_code
 from db import save_url
+import redis
 
 app = FastAPI()
+
+r = redis.Redis(host="redis", port=6379, decode_responses=True)
 
 class URLRequest(BaseModel):
     long_url: str
@@ -12,4 +15,5 @@ class URLRequest(BaseModel):
 def shorten_url(request: URLRequest):
     code = genrate_code()
     save_url(code, request.long_url)
+    r.set(code, request.long_url)
     return {"short_url": f"http://localhost:8001/{code}"}
